07041b941efa41caa56e68bcaa54c216773c3249
Lana Vlasenko <lanachka1993@gmail.com>
2022-05-19 23:50:55 +0300

    PHPDoc

--- src/Logger.php
+++ src/Logger.php
@@ -1,12 +1,27 @@
 <?php
-
+/**
+ * Файл Logger.php
+ * @package lana/logger
+ * @author Lana Vlasenko <lanachka1993@gmail.com>
+ */
 namespace logger;
 
 use Psr\Log\AbstractLogger;
 
-
+/**
+ * Основной класс логгера.
+ * Pеализует интерфейс PSR LoggerInterface. Умеет форматировать и записывать логи в файл.
+ */
 class Logger extends AbstractLogger
 {
+    /**
+     * Метод write сохраняет предварительно отформатированные записи в файл.
+     * @param $logDate
+     * @param $logLevel
+     * @param $message
+     * @param array $context
+     * @return void
+     */
     private function write($logDate, $logLevel, $message, array $context = array())
     {
         $filePath = __DIR__. '/';
@@ -16,6 +31,14 @@ class Logger extends AbstractLogger
         file_put_contents($filePath . $fileName,$logStr, FILE_APPEND );
     }
 
+    /**
+     * Метод format форматирует запись, так как один из параметров массив
+     * @param $logDate
+     * @param $logLevel
+     * @param $message
+     * @param array $context
+     * @return array
+     */
     private function format($logDate, $logLevel, $message, array $context = array())
     {
         $logInfo = [];
@@ -26,6 +49,13 @@ class Logger extends AbstractLogger
         return $logInfo;
     }
 
+    /**
+     * формат записи в файл
+     * @param $level
+     * @param $message
+     * @param array $context
+     * @return void
+     */
     public function log($level, $message, array $context = array())
     {
         $logDate = date('Y-m-d H:i:s');
