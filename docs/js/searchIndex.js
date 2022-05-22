2b131337329f678c449b1719e0eec509302d68d2
Lana Vlasenko <lanachka1993@gmail.com>
2022-05-22 11:36:30 +0300

    Add documentation

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\logger\\Logger",
+            "name": "Logger",
+            "summary": "\u041E\u0441\u043D\u043E\u0432\u043D\u043E\u0439\u0020\u043A\u043B\u0430\u0441\u0441\u0020\u043B\u043E\u0433\u0433\u0435\u0440\u0430.",
+            "url": "classes/logger-Logger.html"
+        },                {
+            "fqsen": "\\logger\\Logger\u003A\u003Awrite\u0028\u0029",
+            "name": "write",
+            "summary": "\u041C\u0435\u0442\u043E\u0434\u0020write\u0020\u0441\u043E\u0445\u0440\u0430\u043D\u044F\u0435\u0442\u0020\u043F\u0440\u0435\u0434\u0432\u0430\u0440\u0438\u0442\u0435\u043B\u044C\u043D\u043E\u0020\u043E\u0442\u0444\u043E\u0440\u043C\u0430\u0442\u0438\u0440\u043E\u0432\u0430\u043D\u043D\u044B\u0435\u0020\u0437\u0430\u043F\u0438\u0441\u0438\u0020\u0432\u0020\u0444\u0430\u0439\u043B.",
+            "url": "classes/logger-Logger.html#method_write"
+        },                {
+            "fqsen": "\\logger\\Logger\u003A\u003Aformat\u0028\u0029",
+            "name": "format",
+            "summary": "\u041C\u0435\u0442\u043E\u0434\u0020format\u0020\u0444\u043E\u0440\u043C\u0430\u0442\u0438\u0440\u0443\u0435\u0442\u0020\u0437\u0430\u043F\u0438\u0441\u044C,\u0020\u0442\u0430\u043A\u0020\u043A\u0430\u043A\u0020\u043E\u0434\u0438\u043D\u0020\u0438\u0437\u0020\u043F\u0430\u0440\u0430\u043C\u0435\u0442\u0440\u043E\u0432\u0020\u043C\u0430\u0441\u0441\u0438\u0432",
+            "url": "classes/logger-Logger.html#method_format"
+        },                {
+            "fqsen": "\\logger\\Logger\u003A\u003Alog\u0028\u0029",
+            "name": "log",
+            "summary": "\u0444\u043E\u0440\u043C\u0430\u0442\u0020\u0437\u0430\u043F\u0438\u0441\u0438\u0020\u0432\u0020\u0444\u0430\u0439\u043B",
+            "url": "classes/logger-Logger.html#method_log"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\logger",
+            "name": "logger",
+            "summary": "",
+            "url": "namespaces/logger.html"
+        }            ]
+);
